0fd31aefca992875d27cea2a72b2cc24caa61c35
Fut0 <52438619+Fut0@users.noreply.github.com>
2025-04-03 18:05:41 +0900

    4/3

--- src/main/java/com/example/moattravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/moattravel/security/WebSecurityConfig.java
@@ -15,27 +15,30 @@ import org.springframework.security.web.SecurityFilterChain;
 
 public class WebSecurityConfig {
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { //public外した
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.authorizeHttpRequests(requests -> requests
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**","/houses","/houses/{id}").permitAll() //すべてのユーザーにアクセスを許可するURL
+				.authorizeHttpRequests((requests) -> requests
+						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/",
+								"/signup/**", "/houses", "/houses/{id}", "/stripe/webhook")
+						.permitAll() //すべてのユーザーにアクセスを許可するURL
 						.requestMatchers("/admin/**").hasRole("ADMIN") //管理者のみアクセスを許可するURL
 						.anyRequest().authenticated() //上記以外のURLはログインが必要(会員または管理者のどちらでもOK)
 				)
-				.formLogin(form -> form
+				.formLogin((form) -> form
 						.loginPage("/login") //ログインページのURL
 						.loginProcessingUrl("/login") //ログインフォームの送信先URL
 						.defaultSuccessUrl("/?loggedIn", true) //ログイン成功時のリダイレクト先URL trueを追加
 						.failureUrl("/login?error") //ログイン失敗時のリダイレクト先URL
 						.permitAll())
-				.logout(logout -> logout
+				.logout((logout) -> logout
 						.logoutSuccessUrl("/?loggedOut") //ログアウト時のリダイレクト先URL
-						.permitAll());
+						.permitAll())
+				.csrf((csrf) -> csrf.ignoringRequestMatchers("/stripe/webhook"));
 		return http.build();
 	}
 
 	@Bean
-	PasswordEncoder passwordEncoder() { //public外した
+	public PasswordEncoder passwordEncoder() { //public
 		return new BCryptPasswordEncoder();
 	}
 }
